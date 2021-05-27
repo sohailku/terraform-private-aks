891e3017ba388c9ce64577af37b7364df078e4a4
sohailku <76937574+sohailku@users.noreply.github.com>
2021-05-27 10:42:10 +0400

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,15 @@
-output "ssh_command" {
-  value = "ssh ${module.jumpbox.jumpbox_username}@${module.jumpbox.jumpbox_ip}"
-}
+# output "ssh_command" {
+#  value = "ssh ${module.jumpbox.jumpbox_username}@${module.jumpbox.jumpbox_ip}"
+# }
 
-output "jumpbox_password" {
-  description = "Jumpbox Admin Passowrd"
-  value       = module.jumpbox.jumpbox_password
-}
+# output "jumpbox_password" {
+ # description = "Jumpbox Admin Passowrd"
+ # value       = module.jumpbox.jumpbox_password
+ # sensitive = true
+# }
+
+#resource "local_file" "kubeconfig" {
+#  depends_on   = [azurerm_kubernetes_cluster.privateaks]
+ # filename     = "kubeconfig"
+ # content      = azurerm_kubernetes_cluster.privateaks.kube_config_raw
+#}
